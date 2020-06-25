bc54ce8f5823307a4b9a52714ed63dfde4843e56
huuphuoc1xx <34543227+huuphuoc1xx@users.noreply.github.com>
2020-06-25 10:17:48 +0700

    create object

--- server/handle/handle.go
+++ server/handle/handle.go
@@ -1,10 +1,19 @@
 package handle
+
 import (
-	"github.com/gin-gonic/gin"
 	"database/sql"
+
+	"github.com/gin-gonic/gin"
 )
+
 var db *sql.DB
 
-func GetPaymentMethod(ctx *gin.context){
-	db.
+func GetPaymentMethod(ctx *gin.context) {
+	conn, err := db.Conn(ctx)
+	if err != nil {
+		ctx.JSON(200, gin.H{"error": "500", "data": {"error": "Can not connect to database"}})
+		return
+	}
+
+	row,err:= conn.QueryContext(ctx,"Select * from")
 }
